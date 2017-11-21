f78b3e62b07fee19f55842df2bd93d2820022ef4
Axel <axosar@gmail.com>
2017-11-21 10:40:06 -0600

    DB Config
    Configuración de base de datos

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    remoteUrl : 'mongodb://imcycmexico:3mcycm2x3c4@ec2-18-220-192-140.us-east-2.compute.amazonaws.com:27017/dummyDB',
+    localUrl: 'mongodb://imcycmexico:3mcycm2x3c4@ec2-18-220-192-140.us-east-2.compute.amazonaws.com:27017/dummyDB'
 };
